2c47ccbd5c51d79820bc0d7cfba510aa42686baf
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-14 18:38:14 -0300

    Exercicio. 10
    Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
    
    Status: Resolvido

---
+++ Exercicio. 10/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao programa de calculo para media ponderada de provas!\n Digite qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            double peso01 = double.Parse(getInput("informe o peso da primeira nota para prosseguir"));
+            double nota01 = double.Parse(getInput("informe a primeira nota para prosseguir"));
+            double peso02 = double.Parse(getInput("informe o peso da segunda nota para prosseguir"));
+            double nota02 = double.Parse(getInput("informe a segunda nota para prosseguir"));
+
+            double mediaAluno = ((peso01 * nota01) + (peso02 * nota02)) / (peso01 + peso02);
+
+            Console.WriteLine($"A media do aluno é de:{mediaAluno}");
+            Console.ReadKey();
+
+            string getInput(string mensagem)
+            {
+                Console.WriteLine(mensagem);
+                string userInput = Console.ReadLine();
+                return userInput;
+            }
+        }
+    }
+}
