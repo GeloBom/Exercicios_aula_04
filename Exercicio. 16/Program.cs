81251b0e85444059a6420e13393d509760800f7c
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-15 02:51:54 -0300

    Exercicio. 16
    Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de impostos. Imprima o salário inicial, o salário com o aumento e o salário final.
    
    Status: Resolvidos

---
+++ Exercicio. 16/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao programa de calculo de  aumento salarial!\nDigite qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            Console.WriteLine("Digite o seu nome para continuar:");
+            string nomeUsuario =  Console.ReadLine();
+
+            double salarioIncial = double.Parse(getInput("informe o seu salario:"));
+
+            double aumentoSalarial = 0.15 * salarioIncial;
+            double salarioPosAumento = aumentoSalarial + salarioIncial;
+            Console.WriteLine($"Parabéns, {nomeUsuario}! Seu aumento salarial é de: {aumentoSalarial:C}");
+
+            double descontoSalarial = 0.08 * salarioPosAumento;
+            double salarioFinal = salarioPosAumento - descontoSalarial;
+            Console.WriteLine($"{nomeUsuario}, seu salário após o aumento é de: {salarioFinal:C}. \nDescontado com os impostos de: {descontoSalarial:C}");
+
+            string getInput(string mensagem)
+            {
+                Console.WriteLine(mensagem);
+                string userInput = Console.ReadLine();
+                return userInput;
+            }
+        }
+    }
+}
