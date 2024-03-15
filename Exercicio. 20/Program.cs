5b98dd3b4bc10ae5fc0c8268b14b7b050e490bbd
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-15 16:22:31 -0300

    Exercicio. 20
    Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar
    
    Status: Resolvido

---
+++ Exercicio. 20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio._20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bem-vindo ao Programa de Ordenação Decrescente!\nPressione qualquer tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+
+                double numero = double.Parse(GetInput("Por favor, insira o numero:"));
+                if (numero % 2 != 0)
+                {
+                    Console.WriteLine($"\nO número {numero} é ímpar.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine($"\nO número {numero} é par.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+
+        static string GetInput(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+    }
+}
