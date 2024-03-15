0491c74e2e632b328149cb25b46252ff2c893652
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-15 01:25:09 -0300

    Exercicio 13.
    A padaria Hotpão vende uma certa quantidade de pães franceses e uma quantidade de broas a cada dia. Cada pãozinho custa R$ 0,12 e a broa custa R$ 1,50. Ao final do dia, o dono quer saber quanto arrecadou com a venda dos pães e broas (juntos), e quanto deve guardar numa conta de poupança (10% do total arrecadado). Você foi contratado para fazer os cálculos para o dono. Com base nestes fatos, faça um algoritmo para ler as quantidades de pães e de broas, e depois calcular os dados solicitados.
    
    Status: Resolvido.

--- Exercicio. 13/Program.cs
+++ Exercicio. 13/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Exercicio._13
+﻿using System.Xml;
+
+namespace Exercicio._13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem vindo ao programa de calculo da padaroa HotPão!\n Digite qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            double paesVendidos = double.Parse(getInput("informe o numero de pães vendidos para prosseguir"));
+            double valorPaes = paesVendidos * 0.12;
+            Console.WriteLine($"O valor arrecadado com a venda de pães foi: {valorPaes}");
+
+            
+            double broaVendidas = double.Parse(getInput("informe o numero de broas vendidas para prosseguir"));
+            double valorBroas = broaVendidas * 1.5;
+            Console.WriteLine($"O valor arrecadado com a venda de broas foi: {valorBroas}");
+
+            double vendaTotal = valorPaes  + valorBroas;
+
+            Console.WriteLine($"O valor total arrecadado:{vendaTotal}");
+            
+            double poupanca = (vendaTotal * 0.1);
+            Console.WriteLine($"O valor a ser reservado para poupança de 10%, deve ser de: {poupanca.ToString("F")}");
+
+            string getInput(string mensagem)
+            {
+                Console.WriteLine(mensagem);
+                string userInput = Console.ReadLine();
+                return userInput;
+            }
         }
     }
 }
