3c42817243b8e3487863b9c087a765893c0ca08a
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-18 12:12:34 -0300

    Exercicio. 28
    Escreva um programa que exiba a tabuada de multiplicação do 1 até o 10. Para isso, o programa deve gerar todas as combinações de multiplicação entre 1 e 10, exibindo o resultado de cada operação na tela.
    Desafio: Para cada número, tente separar em colunas.
    
    Status: Resolvido

--- Exercicio. 28/Program.cs
+++ Exercicio. 28/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem-vindo ao Programa de tabuada em tabelas dos numeros 1 a 10!\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+
+            Console.WriteLine("Tabuada de Multiplicação (1 a 10):\n");
+
+            Console.WriteLine("  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+");
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                Console.Write("  |");
+
+                for (int multiplicando = 1; multiplicando <= 10; multiplicando++)
+                {
+                    int resultado = multiplicador * multiplicando;
+                    Console.Write($" {resultado,3} |");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("  +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+");
+            }
         }
     }
 }
