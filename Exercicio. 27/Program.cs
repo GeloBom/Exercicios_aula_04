8cc9c42eef988fcdb5c22dfdaa48af768bc94e6e
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-03-18 11:46:33 -0300

    Exercicio. 27
    Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo os números múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para números que são múltiplos de ambos, use "FizzBuzz".
    
    Status: Resolvido

--- Exercicio. 27/Program.cs
+++ Exercicio. 27/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem-vindo ao Programa de numeração (1 a 100) com os nomes fizz para multiplos por 3, buzz para multiplos por 5 e FizzBuzz para multiplos pelos 2 numeros.\nPressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                if (numero % 3 == 0 && numero % 5 == 0)
+                {
+                    Console.WriteLine($"{numero} = FizzBuzz (Multiplo de 3 e 5)");
+                }
+                else if (numero % 3 == 0)
+                {
+                    Console.WriteLine($"{numero} = Fizz (Multiplo por 3)");
+                }
+                else if (numero % 5 == 0)
+                {
+                    Console.WriteLine($"{numero} = Buzz (Multiplo por 5)");
+                }
+                else
+                {
+                    Console.WriteLine(numero);
+                }
+            }
         }
     }
 }
---
+++ Exercicio. 28/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio._28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
